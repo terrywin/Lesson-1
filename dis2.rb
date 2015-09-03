5e4fe5df7e20751913c60e305cd848eaa211c841
Terrywin <ferril.black@ya.ru>
2015-09-04 00:56:30 +0300

    fourth  commit

--- dis2.rb
+++ dis2.rb
@@ -9,7 +9,7 @@ c = gets.chomp
 if d < 0 
 	puts "Корней нет!"
 elsif d == 0 
-
+#
  	 puts "Дискриминант равен 0 ,корни равны!x1,x2 = #{(-b.to_f) / (2 * a.to_f)}" 
  	else 
  	 puts "Дискриминант больше 0 ,x1 = #{(-b.to_f + Math.sqrt(d.to_f)) / (2*a.to_f)},x2 = #{(-b.to_f - Math.sqrt(d.to_f)) /(2*a.to_f)}"
