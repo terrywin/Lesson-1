0f947f79e9107367389c10c2def19df6af5222ef
Terrywin <ferril.black@ya.ru>
2015-09-04 01:09:58 +0300

    5commit

--- dis2.rb
+++ dis2.rb
@@ -8,8 +8,8 @@ c = gets.chomp
  puts "d = #{d}"
 if d < 0 
 	puts "Корней нет!"
-elsif d == 0 
-#
+elsif d == 0 #
+##
  	 puts "Дискриминант равен 0 ,корни равны!x1,x2 = #{(-b.to_f) / (2 * a.to_f)}" 
  	else 
  	 puts "Дискриминант больше 0 ,x1 = #{(-b.to_f + Math.sqrt(d.to_f)) / (2*a.to_f)},x2 = #{(-b.to_f - Math.sqrt(d.to_f)) /(2*a.to_f)}"
