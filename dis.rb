d9003dd1f36ccc481ca0ca41edc868defc85a260
terrywin <ferril.black@ya.ru>
2015-09-03 23:39:54 +0300

    Update dis.rb

--- dis.rb
+++ dis.rb
@@ -4,7 +4,7 @@ puts "Введите значение b"
 b = gets.chomp
 puts "Введите значение c"
 c = gets.chomp
-puts "d = #{d = ((b.to_i)**2 )- ((a.to_i * c.to_i) * 4) } "
+"d = #{d = ((b.to_i)**2 )- ((a.to_i * c.to_i) * 4) } "
 if d < 0 
 	puts "Корней нет!"
 elsif d == 0 
